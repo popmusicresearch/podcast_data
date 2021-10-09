202a94c3c3e402494b88fe93f9b542e1cb6bb3e1
Craig Hamilton <craig.hamilton@gmail.com>
2021-10-09 17:39:58 +0100

    Add reviews data

--- code/04 - Get Reviews data.R
+++ code/04 - Get Reviews data.R
@@ -75,3 +75,111 @@ country <- c("Albania", "Algeria", "Angola", "Anguilla", "Antigua and Barbuda",
 
 apple <- as.data.frame(cbind(code, country))
 write_rds(apple, 'data/apple_store_codes.rds')
+
+
+#Gather Reviews and Ratings for the 1st podcast from each of the 155 national stores. 
+pod_id <- podcast_links$apple_id[[1]]
+podcast <- podcast_links$podcast[[1]]
+
+for (i in 1:nrow(apple)) {
+  try({
+    page = 1
+    x <- apple$code[[i]]
+    y <- apple$country[[i]]
+    print(paste("Now looking at", y, "for", podcast, sep = " "))
+    reviews_test <- getReviews(pod_id, x, page) 
+    reviews_test$country <- x
+    reviews_test$podcast <- podcast
+    reviews_test$pod_id <- pod_id
+    reviews_test$review_page <- page 
+    reviews_test <- reviews_test[, c(9:11, 1:8)]
+    reviews <- rbind(reviews, reviews_test)
+    if (nrow(reviews_test) == 49)
+      repeat {
+        try({
+          rm(reviews_test)
+          wait_time <- sample(10:20, 1)
+          print(paste("Waiting", wait_time, "seconds before checking page", page + 1, "for", y, "for", podcast, sep = " "))
+          Sys.sleep(wait_time)
+          page = page + 1
+          if (page > 10) break
+          reviews_test <- getReviews(pod_id, x , page) 
+          reviews_test$country <- x
+          reviews_test$podcast <- podcast
+          reviews_test$pod_id <- pod_id
+          reviews_test$review_page <- page 
+          reviews_test <- reviews_test[, c(9:11, 1:8)]
+          reviews <- rbind(reviews, reviews_test)
+          if (nrow(reviews_test) < 49) break
+        })#, silent = T)
+      }
+  }, silent = T)
+}
+
+#Remove the duplicate reviews - these were generated in the example step earlier in the script.
+reviews <- reviews[!duplicated(reviews$Review), ]
+
+#Now gather Reviews and Rating for the remaining podcasts in the chart
+for (i in 2:nrow(podcast_links)) {
+  pod_id <- podcast_links$apple_id[[i]]
+  podcast <- podcast_links$podcast[[i]]
+  for (j in 1:nrow(apple)) {
+    try({
+      page = 1
+      x <- apple$code[[j]]
+      y <- apple$country[[j]]
+      print(paste("Now looking at page", page, "for", y, "for", podcast, sep = " "))
+      reviews_test <- getReviews(pod_id, x, page) 
+      reviews_test$country <- x
+      reviews_test$podcast <- podcast
+      reviews_test$pod_id <- pod_id
+      reviews_test$review_page <- page 
+      reviews_test <- reviews_test[, c(9:11, 1:8)]
+      reviews <- rbind(reviews, reviews_test)
+      if (nrow(reviews_test) == 49)
+        repeat {
+          try({
+            rm(reviews_test)
+            wait_time <- sample(10:20, 1)
+            print(paste("Waiting", wait_time, "seconds before checking page", page + 1, "for", y, "for", podcast, sep = " "))
+            Sys.sleep(wait_time)
+            page = page + 1
+            if (page > 10) break ##Page numbers > 10 return HTTP error
+            reviews_test <- getReviews(pod_id, x , page) 
+            reviews_test$country <- x
+            reviews_test$podcast <- podcast
+            reviews_test$pod_id <- pod_id
+            reviews_test$review_page <- page 
+            reviews_test <- reviews_test[, c(9:11, 1:8)]
+            reviews <- rbind(reviews, reviews_test)
+            if (nrow(reviews_test) < 49) break
+          })#, silent = T)
+        }
+    }, silent = T)
+  }
+}
+
+#Remove any duplicates
+reviews <- reviews[!duplicated(reviews$Review), ]
+
+#Check that there are reviews for each of the podcasts in the orginal chart
+length(unique(reviews$podcast))
+## [1] 2
+#It appears that only 2 of the 3 podcasts were checking are present.
+
+#Find the missing podcast[s]
+reviewed <- unique(reviews$pod_id)
+reviews_missing <- podcast_links %>%
+  filter(!apple_id %in% reviewed )
+unique(reviews_missing$podcast)
+#[1] "The Bright Magic Podcast"
+
+#From looking at the Apple page for The Bright Magic Podcast, this appears to be very new and 
+#no reviews have yet been posted. 
+
+#Write out data
+write_rds(reviews, "data/reviews.rds")
+
+#Optional housekeeping
+rm(podcast_links, episode_data, apple, reviews, reviews_missing, reviews_test, code, country, i, j,
+   page, pod_id, podcast, reviewed, wait_time, x, y)
